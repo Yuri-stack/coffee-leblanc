8b0f173417b8da445e24e0259ad6e8357eff5ee6
Yuri-Stack <yurioliveirasilva1999@gmail.com>
2022-07-19 12:11:55 -0300

    Styles: Configurando Estilos

---
+++ src/styles/global.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    :focus{
+        outline: 0;
+        box-shadow: 0 0 0 2px ${props => props.theme["yellow-dark"]};
+    }
+
+    body{
+        font: 400 1rem/130% 'Roboto', sans-serif;
+        background: ${props => props.theme["background"]};
+        color: ${props => props.theme["base-text"]};
+    }
+
+    header{
+        font: 700 1.125rem/130% 'Baloo 2', cursive;
+    }
+`
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,11 @@
+export const defaultTheme = {
+    'yellow-light': '#F2E852',
+    'yellow-dark':'#C47F17',
+
+    'red':'#D92323',
+    'red-dark':'#732424',
+
+    'base-text':'#E6E5E5',
+
+    'background':'#0d1117'
+}
